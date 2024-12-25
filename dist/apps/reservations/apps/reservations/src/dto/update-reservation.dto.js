cf813295f538275b2e5ffa0ff87661da9e1383ac
webcreation-dev <adjilan2403@gmail.com>
2024-12-25 02:13:53 +0100

    ficing bugs

--- dist/apps/reservations/apps/reservations/src/dto/update-reservation.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateReservationDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_reservation_dto_1 = require("./create-reservation.dto");
-class UpdateReservationDto extends (0, mapped_types_1.PartialType)(create_reservation_dto_1.CreateReservationDto) {
-}
-exports.UpdateReservationDto = UpdateReservationDto;
-//# sourceMappingURL=update-reservation.dto.js.map
